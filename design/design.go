2b320ca76bf292fd4756c3a5057746da37baa3a8
kishimoto-banana <drehbleistift@gmail.com>
2020-01-24 14:46:27 +0900

    Add design

---
+++ design/design.go
@@ -0,0 +1,28 @@
+package design
+
+import (
+	. "goa.design/goa/v3/dsl"
+)
+
+var _ = API("example-api", func() {
+	Title("exmaple api")
+	Descriptino("Service for example")
+	Server("example-api", func() {
+		Host("localhost", func() {
+			URI("http://localhost:8000") 
+		})
+	})
+})
+
+var _ = Service("ping", func() {
+	Description("Ping/Pong service")
+	Method("Return pong", func() {
+		Result(String)
+		HTTP(func() {
+			GET("/ping")
+			Response(StatusOK, func() {
+				ContentType("text/html")
+			})
+		})
+	})
+})
